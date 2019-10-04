0a5f5076f516ad90b55360568b26c44dd44aaa2e
Sergio Garcia <sergiogarciamf@gmail.com>
2019-10-04 11:24:34 -0400

    add basic files

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a companion repository to [modcell-hpc](https://github.com/TrinhLab/modcell-hpc) used to store results and analysis.
+
+-
---
+++ mhr-root.m
@@ -0,0 +1,3 @@
+% Dummy file to identify this directory from any script using:
+% fileparts(which('filename.m'))
+% this directory must be in the matlab path for the above to work.
